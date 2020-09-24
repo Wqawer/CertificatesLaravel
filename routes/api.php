b42c62c9db8004d6b29955f7d7d856e512d31c29
michal <m.wqawer@gmail.com>
2020-09-24 16:20:21 +0200

    authorisation

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CertificateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,10 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::post('me', 'AuthController@me');
 });
 
-Route::get('/certificates', [CertificateController::class, 'index']) ->name('certificates.index');
+Route::get('/certificates', [CertificateController::class, 'index']) ->middleware('jwt.auth')->name('certificates.index');
 Route::get('/certificates/{certificate}', [CertificateController::class, 'show']) ->name('certificates.show');
 Route::post('/certificates', [CertificateController::class, 'store'])->name('certificates.store');
+
